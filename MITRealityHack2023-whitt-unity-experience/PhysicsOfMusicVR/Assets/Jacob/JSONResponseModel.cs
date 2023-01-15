d4d95331881d91ab1c5bb20451734c7bd9644110
Edward C. Deaver, IV <EdwardDeaver@users.noreply.github.com>
2023-01-15 13:08:08 -0500

    Whitts finished code

--- MITRealityHack2023-whitt-unity-experience/PhysicsOfMusicVR/Assets/Jacob/JSONResponseModel.cs
+++ MITRealityHack2023-whitt-unity-experience/PhysicsOfMusicVR/Assets/Jacob/JSONResponseModel.cs
@@ -12,7 +12,7 @@ public class JSONResponseModel : MonoBehaviour
     public bool NOTE1;
     public bool NOTE2;
     public bool NOTE3;
-    public bool NOTE4;
+    //public bool NOTE4;
     public bool STRUM;
     public bool Fret1P23;
     public bool Fret2P22;
@@ -29,52 +29,52 @@ public class JSONResponseModel : MonoBehaviour
     public TMP_Text BUTTON2TEXT;
     public TMP_Text BUTTON3TEXT;
     public TMP_Text BUTTON4TEXT;
-    public TMP_Text STRUM_TEXT;
+    // public TMP_Text STRUM_TEXT;
 
     void Update()
     {
 
         FREQTEXT.text = this.FREQ.ToString();
-        Debug.Log(Pot1P33.ToString());
+       // Debug.Log(Pot1P33.ToString());
 
         AMPTEXT.text = this.AMP.ToString();
-        Debug.Log(Pot2P25.ToString());
+       // Debug.Log(Pot2P25.ToString());
 
         LFOTEXT.text = this.LFO.ToString();
-        Debug.Log(Pot3P26.ToString());
+       // Debug.Log(Pot3P26.ToString());
 
         BUTTON1TEXT.text = this.NOTE1.ToString();
         BUTTON2TEXT.text = this.NOTE2.ToString();
         BUTTON3TEXT.text = this.NOTE3.ToString();
-        BUTTON4TEXT.text = this.NOTE4.ToString();
+        //BUTTON4TEXT.text = this.NOTE4.ToString();
 
-        STRUM_TEXT.text = this.STRUM.ToString(); 
+        //STRUM_TEXT.text = this.STRUM.ToString(); 
     }
 
     public void onMessage(string savedData)
     {
-        Debug.Log("Debug Got the Message!");
+        //Debug.Log("Debug Got the Message!");
         Debug.Log(savedData);
         try
         {
             JsonUtility.FromJsonOverwrite(savedData, this);
             this.AMP = this.Pot1P33;
-            this.AMP = Mathf.Lerp(0.0f, 1.0f, this.AMP);
+            this.AMP = Mathf.Lerp(0.1f, 2.0f, this.AMP);
             this.LFO = this.Pot2P25;
-            this.LFO = Mathf.Lerp(0.0f, 16.0f, this.AMP);
+            this.LFO = Mathf.Lerp(0.0f, 16.0f, this.LFO);
             this.FREQ = this.Pot3P26;
-            this.FREQ = Mathf.Lerp(0.0f, 2048.0f, this.FREQ);
-            this.NOTE1 = this.Fret1P23;
-            this.NOTE2 = this.Fret2P22;
-            this.NOTE3 = this.Fret3P21;
-            this.STRUM = this.Fret4P19; 
-            if (this.Strum1P18 == false)
-            {
-                this.NOTE1 = false;
-                this.NOTE2 = false;
-                this.NOTE3 = false;
-                this.NOTE4 = false;
-            }
+            this.FREQ = Mathf.Lerp(1.0f, 2.0f, this.FREQ);
+            this.NOTE1 = !(this.Fret1P23);
+            this.NOTE2 = !(this.Fret2P22);
+            this.NOTE3 = !(this.Fret3P21);
+            this.STRUM = !(this.Fret4P19); 
+            // if (this.Strum1P18 == false)
+            // {
+            //     this.NOTE1 = false;
+            //     this.NOTE2 = false;
+            //     this.NOTE3 = false;
+            //     this.NOTE4 = false;
+            // }
         }
         catch (Exception e)
         {
--- MITRealityHack2023-whitt-unity-experience/PhysicsOfMusicVR/Assets/Scripts/ESPAVControl.cs
+++ MITRealityHack2023-whitt-unity-experience/PhysicsOfMusicVR/Assets/Scripts/ESPAVControl.cs
@@ -7,8 +7,12 @@ using UnityEngine.VFX;
 public class ESPAVControl : MonoBehaviour
 {
     public JSONResponseModel _jsonResponseModel;
-    public List<GameObject> _audioObjects;
+    public List<Csound.TableMorph.Theremin.theraminSynth> _theraminSynths;
+    public List<AudioSource> _audioObjects;
     public List<VisualEffect> _particleWaves;
+
+    public string _vfxStopEvent = "OnStop";
+    public string _vfxPlayEvent = "OnPlay";
     
     // Start is called before the first frame update
     void Start()
@@ -17,7 +21,14 @@ public class ESPAVControl : MonoBehaviour
         {
             for (int i = 0; i < _audioObjects.Count; i++)
             {
-                _audioObjects[i].SetActive(false);
+                _audioObjects[i].enabled = false;
+            }
+        }
+        if(_particleWaves.Count > 0)
+        {
+            for (int i = 0; i < _particleWaves.Count; i++)
+            {
+                _particleWaves[i].SendEvent(_vfxStopEvent);
             }
         }
     }
@@ -25,21 +36,60 @@ public class ESPAVControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        LinkAudioSources();
+        LinkVFX();
+    }
+
+    void LinkAudioSources()
+    {
+        _audioObjects[0].enabled = _jsonResponseModel.NOTE1;
+        _audioObjects[1].enabled = _jsonResponseModel.NOTE2;
+        _audioObjects[2].enabled = _jsonResponseModel.NOTE3;
+        //_audioObjects[3].enabled = _jsonResponseModel.NOTE4;
+    }
+
+    void LinkVFX()
+    {
+        if(_theraminSynths.Count >0 && _particleWaves.Count >0)
+        {
+            for(int i = 0; i < _particleWaves.Count; i++)
+            {
+                _particleWaves[i].SetFloat("Frequency", _theraminSynths[i].frequencyField);
+                _particleWaves[i].SetFloat("Amplitude", _theraminSynths[i].amplitudeField);
+                _particleWaves[i].SetFloat("LFO", _theraminSynths[i].lfoField);
+            }
+        }
         if(_jsonResponseModel.NOTE1)
         {
-            _audioObjects[0].SetActive(true);
+            _particleWaves[0].SendEvent(_vfxPlayEvent);
+        }
+        if(!_jsonResponseModel.NOTE1)
+        {
+            _particleWaves[0].SendEvent(_vfxStopEvent);
         }
         if(_jsonResponseModel.NOTE2)
         {
-            _audioObjects[1].SetActive(true);
+            _particleWaves[1].SendEvent(_vfxPlayEvent);
         }
-        if(_jsonResponseModel.NOTE2)
+        if(!_jsonResponseModel.NOTE2)
         {
-            _audioObjects[2].SetActive(true);
+            _particleWaves[1].SendEvent(_vfxStopEvent);
         }
-        if(_jsonResponseModel.NOTE2)
+        if(_jsonResponseModel.NOTE3)
         {
-            _audioObjects[3].SetActive(true);
+            _particleWaves[2].SendEvent(_vfxPlayEvent);
         }
+        if(!_jsonResponseModel.NOTE3)
+        {
+            _particleWaves[2].SendEvent(_vfxStopEvent);
+        }
+        // if(_jsonResponseModel.NOTE4)
+        // {
+        //     _particleWaves[3].SendEvent(_vfxPlayEvent);
+        // }
+        // if(!_jsonResponseModel.NOTE4)
+        // {
+        //     _particleWaves[3].SendEvent(_vfxStopEvent);
+        // }
     }
 }
